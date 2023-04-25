26e5d51e4f101873500cab70b79769711e3777eb
harishsekar1402 <srisaiharish1423@gmsil.com>
2023-04-25 12:01:54 +0530

    added params

--- aboutus.js
+++ aboutus.js
@@ -1,6 +1,30 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 router.get('/',(request,response)=>{
-    response.send("Hello im About page")
+    response.send("Heelo jsdh")
 })
-module.exports = router
+
+const Services = [
+    {
+        id: 1,
+        name: "Web Developer"
+    },
+    {
+        id: 2,
+        name: "Game Developer"
+    },
+    {
+        id: 3,
+        name: "Developer"
+    }
+];
+
+router.get("/:no", (request, response) => {
+    const Service_id = Number(request.params.no);
+    const findService = Services.find(service => service.id === Service_id);
+    console.log(findService)
+});
+module.exports = router;
+
+
+
